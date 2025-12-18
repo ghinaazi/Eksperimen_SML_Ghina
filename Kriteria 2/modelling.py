44d674987cba3b745d6ae8680744a3dbcf5e7901
ghinaazi <inayahzulfaghina@gmail.com>
2025-12-18 20:28:25 +0700

    Create modelling.py

---
+++ Kriteria 2/modelling.py
@@ -0,0 +1,30 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+train_path = "data_preprocessing/train.csv"
+test_path = "data_preprocessing/test.csv"
+
+train_df = pd.read_csv(train_path)
+test_df = pd.read_csv(test_path)
+
+X_train = train_df.drop("Class", axis=1)
+y_train = train_df["Class"]
+
+X_test = test_df.drop("Class", axis=1)
+y_test = test_df["Class"]
+
+# Enable MLflow autolog
+mlflow.sklearn.autolog()
+
+# Training model
+with mlflow.start_run():
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    
+    # Evaluasi akurasi pada test set
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"Test Accuracy: {acc}")
